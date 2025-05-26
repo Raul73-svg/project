27968dea0fee220c75e41f1b964e13e433f64c49
Raul73-svg <raulramossuarez.91@gmail.com>
2025-05-26 13:10:58 +0100

    formulario.php

--- formulario.php
+++ formulario.php
@@ -1,15 +1,15 @@
 <?php
-require 'database.php';
+require 'conexion.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST["create"])) {
 
-        if (!empty($_POST["nombre"]) && !empty($_POST["nombre"]) && !empty($_POST["apellidos"]) && !empty($_POST["ciudad"])) {
-            createpersona($_POST["nombre"], $_POST["edad"]);
+        if (!empty($_POST["dni"]) && !empty($_POST["nombre"]) && !empty($_POST["apellidos"]) && !empty($_POST["ciudad"])) {
+            createpersona($_POST["dni"], $_POST["nombre"], $_POST["apellidos"], $_POST["ciudad"]);
         } else {
             echo "<script>alert('todos los datos son requeridos');</script>";
         }
-}
+}}
 
 $personas = getpersonas();
 ?>
@@ -21,11 +21,11 @@ $personas = getpersonas();
     <title>Document</title>
 </head>
 <body>
-    <form action="conexion.php" method="post">
+    <form  method="post">
         <h2>formulario</h2>
         <p>
-            <label for="dni">DNI</label>
-            <input type="text" name="dni" maxlength="9"/>
+            <label>DNI<input type="text" name="dni" maxlength="9"/></label>
+            
         </p>
         <p>
             <label>nombre<input type="text" name="nombre"/></label>
@@ -34,11 +34,22 @@ $personas = getpersonas();
             <label>apellidos<input type="text" name="apellidos"/></label> 
         </p>
         <p>
-            <label>ciudad<input type="text" name="ciduad"/></label>
+            <label>ciudad<input type="text" name="ciudad"/></label>
         </p>
-        <input type="submit" value="enviar"/>
+        <input type="submit" name="create" value="enviar"/>
     </form>
-     <?php foreach ($personas as $persona): ?>
+    <h2>personas</h2>
+    <table>
+    <thead>
+                <tr>
+                    <td>dni</td>
+                    <td>nombre</td>
+                    <td>apellidos</td>
+                    <td>ciudad</td>
+                </tr>
+    </thead>
+    <tbody>
+         <?php foreach ($personas as $persona): ?>
                 <tr>
                     <td><?php echo $persona["dni"]; ?></td>
                     <td><?php echo $persona["nombre"]; ?></td>
@@ -46,5 +57,7 @@ $personas = getpersonas();
                     <td><?php echo $persona["ciudad"]; ?></td>
                 </tr>
                 <?php endforeach; ?>
+    </tbody>
+    </table>
 </body>
 </html>
