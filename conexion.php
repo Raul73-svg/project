04b2eb468cc4c167765c94486babfce6b136ac6b
Raul73-svg <raulramossuarez.91@gmail.com>
2025-05-26 13:11:23 +0100

    conexion.php

--- conexion.php
+++ conexion.php
@@ -1,28 +1,30 @@
 <?php
 
-$mysqli = new mysqli('localhost','root','1234', 'base de datos')or die(mysgl_error($mysqli));
+$mysqli = new mysqli('localhost','root','1234', 'personas');
+if ($mysqli->connect_error) {
+    die("Conexión fallida: " . $mysqli->connect_error);
+}
 
-
-function insertar($dni, $nombre, $apellidos, $ciudad){
+function createpersona($dni, $nombre, $apellidos, $ciudad){
     global $mysqli;
-    $stmt = $mysqli->prepare("INSERT INTO project (dni, nombre, apellidos, ciudad) VALUES '(?, ?, ?, ?)");
-    $stmt->bind_param("si",$dni, $nombre, $apellidos, $ciudad); 
+    $stmt = $mysqli->prepare("INSERT INTO persona (dni, nombre, apellidos, ciudad) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("ssss",$dni, $nombre, $apellidos, $ciudad); 
     $stmt->execute();
-    $stmt->closes();
+    $stmt->close();
 }
  
 
-function getpersonas(){
+function getpersonas() {
     global $mysqli;
-    $result = $mysqli-> ("SELECT dni, nombre, apellidos, ciudad FROM project");
-    return $return->fetch_all(MYSQLI_ASSOC);
+    $result = $mysqli->query("SELECT dni, nombre, apellidos, ciudad FROM persona");
+    return $result->fetch_all(MYSQLI_ASSOC);
 }
 
 
 function getpersona($dni){
     global $mysqli;
-    $stmt = $mysqli->prepare("SELECT dni, nombre, apellidos, ciudad FROM project WHERE dni = ?");
-    $stmt->bind_param("i", $dni);
+    $stmt = $mysqli->prepare("SELECT dni, nombre, apellidos, ciudad FROM persona WHERE dni = ?");
+    $stmt->bind_param("s", $dni);
     $stmt->execute();
     $result = $stmt->get_result();
     return $result->fetch_assoc();
